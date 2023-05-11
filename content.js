ee4fa3b363da406bf7b084baa48585b60aeb5e14
706718242 <124694760+706718242@users.noreply.github.com>
2023-05-12 01:04:06 +0800

    Update content.js

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-
+var searchText = ["1t3", "1t4", "2t3", "2t4", "3t3", "3t4", "7t6"];
 window.onload = (event) => {
 console.log('page is fully loaded');
 if(sessionStorage.getItem("flag")=="1"){ cleano();}
@@ -20,7 +20,7 @@ if(pi==ps){
 if(sessionStorage.getItem("flag")!="1"){
 
  sessionStorage.setItem("flag", "1");
- alert ("只显示34区");
+ alert ("只显示当前区");
  cleano();
 
  
@@ -35,7 +35,7 @@ if(sessionStorage.getItem("flag")!="1"){
  
 function cleano() {
  
-let searchText = ["1t3", "1t4", "2t3", "2t4", "3t3", "3t4", "7t6"];
+
 
  var table = document.getElementById("OnLine");
  var cells = table.getElementsByTagName("tr");
