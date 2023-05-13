3d76692ec3717c6d24ec4bed06bf5ec04edd981d
706718242 <124694760+706718242@users.noreply.github.com>
2023-05-13 21:40:07 +0800

    Update content.js

--- content.js
+++ content.js
@@ -6,11 +6,11 @@ console.log('page is fully loaded');
  var h1,m;
  h1=h.getHours();
  m=h.getMinutes();
- if((h1==7||h1==19)&&m==50){
+ if((h1==7||h1==19)&&m>=50){
   sessionStorage.setItem("flag", "0");
- console.log(i.getHours());}
+ }
  
-if(sessionStorage.getItem("flag")=="1"){ cleano();}
+if(sessionStorage.getItem("flag")=="1"){ co(); cf();}
 };
 
 document.addEventListener('keydown', (event) => {
@@ -43,20 +43,76 @@ sessionStorage.setItem("user", prompt ());}
 });
 
  
-function cleano() {
+function co() {
 
  var table = document.getElementById("OnLine");
  var cells = table.getElementsByTagName("tr");
 
  for(var j=0;j<searchText.length;j++){
- for (var i = 0; i < cells.length; i++) {
- var cellText = cells[i].innerHTML.toLowerCase(); // 将单元格文本转换为小写字母
- if (cellText.indexOf(searchText[j].toLowerCase()) !== -1) {// 将搜索文本转换为小写字母
+ for (var i = 1; i < cells.length; i++) {
+ var Text = cells[i].getElementsByTagName("td")[0].innerText.toLowerCase(); // 将单元格文本转换为小写字母
+ if (Text.indexOf(searchText[j]) != -1) {
   
   document.getElementById("OnLine").deleteRow(i);
    i--;
    
   }
  }
+ }}
+
+
+ var cfd=[];
+var a=0,b=0,c=0;
+var t=0;//找到次数
+function cf() {
+
+ var table = document.getElementById("OnLine");
+ var cells = table.getElementsByTagName("tr");
+
+
+ for (var i = 1; i < cells.length; i++) {
+ cfd[i] = cells[i].getElementsByTagName("td")[0].innerText; 
+ cff(cfd[i]);
+ if(c>0&&c<0.9){ 
+  
+    cells[i].style.backgroundColor = "red";
+                  //找到要插入行的位置
+   
+    // table.insertRow(0);
+ 
+     //cell[1].innerHTML = cells[i].innerHTML;
+    // table.deleteRow(i);
+               
+               
+               }
+  else{cells[i].style.backgroundColor = "#ffc107"; }
+  if(c==0){cells[i].style.backgroundColor = "";}
  }
-}
+ }
+
+function cff( cfff) {
+ a=0;b=0;c=0;
+ var table = document.getElementById("OnMachine");
+ var cells = table.getElementsByTagName("tr");
+var aa,bb;
+ for (var i = 1; i < cells.length; i++) {
+ var Text = cells[i].getElementsByTagName("td")[0].innerText; 
+ if (Text.indexOf(cfff) != -1) {
+   aa=parseInt(cells[i].getElementsByTagName("td")[3].innerText); 
+   bb=parseInt(cells[i].getElementsByTagName("td")[4].innerText);
+ 
+   a=aa;
+   b=bb;
+    console.log(a);
+   console.log(b);
+   c=b/a;
+  console.log(c);
+
+  //parseInt();
+  }
+
+  
+ }
+
+ 
+ }
